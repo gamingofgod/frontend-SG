21f1f1579b3da20920b8e00dd587eea14d99e094
juan naranjo <juanda01052@hotmail.com>
2023-03-13 14:49:19 -0500

    carpetas para organisar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/login/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div className="text-4xl">Sample</div>} />
+        <Route path="/" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    
+
+    return (
+        <div className="h-screen bg-amber-300">
+            <h1 className="text-9xl">HOLA cOMO Etsa</h1>
+        </div>
+    )
+}
